962dc0c5a7d63aa9edbba722c6bce50b605fdb60
ShutovKS <i@shutovks.ru>
2023-07-27 12:20:26 +0300

    Added services container

---
+++ src/Visual novel/Assets/Scripts/Infrastructure/Services/ServicesContainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infrastructure.Services
+{
+    public static class ServicesContainer
+    {
+        public static void SetUp(params object[] objects)
+        {
+            foreach (var variable in objects)
+            {
+                foreach (var interfaceType in variable.GetType().GetInterfaces())
+                {
+                    _classesDictionary.Add(interfaceType, variable);
+                }
+            }
+        }
+
+        private static Dictionary<Type, object> _classesDictionary = new();
+
+        public static T GetService<T>() where T : class
+        {
+            return _classesDictionary.TryGetValue(typeof(T), out var TClass)
+                ? TClass as T
+                : throw new Exception($"No class {typeof(T)}");
+        }
+    }
+}
