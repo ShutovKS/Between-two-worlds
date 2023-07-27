1c30abcc89e2c0094ee84f39859c8a7014b9e998
ShutovKS <i@shutovks.ru>
2023-07-27 12:21:08 +0300

    Added bootstrap and services initialize

---
+++ src/Visual novel/Assets/Scripts/Infrastructure/Bootstrap/Bootstrape.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Services;
+using Infrastructure.Services.AssetsAddressables;
+using Infrastructure.Services.JsonLoad;
+using Infrastructure.Services.UIFactory;
+using UnityEngine;
+
+namespace Infrastructure.Bootstrap
+{
+    public class Bootstrap : MonoBehaviour
+    {
+        private void Awake()
+        {
+            ServicesInitialize();
+        }
+
+        private void ServicesInitialize()
+        {
+            var assetsAddressablesProvider = new AssetsAddressablesProvider();
+            var uiFactory = new UIFactory(assetsAddressablesProvider);
+            var jsonLoad = new JsonLoad();
+
+            ServicesContainer.SetUp(assetsAddressablesProvider, uiFactory, jsonLoad);
+        }
+    }
+}
