6f130771823228da693d6d21585f36279fc93e87
ShutovKS <i@shutovks.ru>
2023-07-29 00:04:58 +0300

    Added persistent data

---
+++ src/Visual novel/Assets/Scripts/Infrastructure/Services/PersistentData/IPersistentDataService.cs
@@ -0,0 +1,10 @@
+﻿using Data.Dynamic;
+
+namespace Infrastructure.Services.PersistentData
+{
+    public interface IPersistentDataService
+    {
+        DynamicData DynamicData { get; }
+        void SetDynamicData(DynamicData data);
+    }
+}
---
+++ src/Visual novel/Assets/Scripts/Infrastructure/Services/PersistentData/PersistentDataService.cs
@@ -0,0 +1,14 @@
+﻿using Data.Dynamic;
+
+namespace Infrastructure.Services.PersistentData
+{
+    public class PersistentDataService : IPersistentDataService
+    {
+        public DynamicData DynamicData { get; private set; }
+
+        public void SetDynamicData(DynamicData data)
+        {
+            DynamicData = data;
+        }
+    }
+}
