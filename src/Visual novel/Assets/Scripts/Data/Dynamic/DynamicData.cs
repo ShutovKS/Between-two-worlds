87bbcee7404e34982851f958ed088e0c6bdd0f30
ShutovKS <i@shutovks.ru>
2023-08-01 19:09:46 +0300

    Removed language current in dynamic data

--- src/Visual novel/Assets/Scripts/Data/Dynamic/DynamicData.cs
+++ src/Visual novel/Assets/Scripts/Data/Dynamic/DynamicData.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using Data.Dynamic.Language;
 
 namespace Data.Dynamic
 {
     [Serializable]
     public class DynamicData
     {
-        public DynamicData()
-        {
-            LanguageCurrent = new LanguageCurrent();
-        }
-
-        public LanguageCurrent LanguageCurrent;
     }
 }
