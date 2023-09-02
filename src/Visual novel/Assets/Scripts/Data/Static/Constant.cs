2842d0659419f13f04c020f6a1f410fcb0b65669
ShutovKS <i@shutovks.ru>
2023-09-02 21:29:35 +0300

    Added parameters constants

---
+++ src/Visual novel/Assets/Scripts/Data/Static/Constant.cs
@@ -0,0 +1,9 @@
+﻿namespace Data.Static
+{
+    public static class Constant
+    {
+        public const string KEY_ID_DIALOGUE_FOR_PLAYER_PREFS = "KeyIDDialogueForPlayerPrefs";
+        public const string DIALOG_START_ID = "Start";
+        public const string DIALOG_END_ID = "End";
+    }
+}
