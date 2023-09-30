36aad78643a75d62bc98aba4567121cc3defb5b0
ShutovKS <schutow.kir@yandex.ru>
2023-09-30 11:21:13 +0300

    Added LastWordsUI model

--- src/Visual novel/Assets/Scripts/UI/LastWords/LastWordsUI.cs
+++ src/Visual novel/Assets/Scripts/UI/LastWords/LastWordsUI.cs
@@ -1,7 +1,22 @@
-﻿namespace UI.LastWords
+﻿using Data.Localization.UILocalisation;
+using Infrastructure.Services.LocalizationUI;
+using TMPro;
+using Units.Tools;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace UI.LastWords
 {
-    public class LastWordsUI
+    public class LastWordsUI : MonoBehaviour, ILocalizableUI
     {
-        
+        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private Button _backButton;
+        [SerializeField] private GameObject _panel;
+
+        public void SetText(string text) => _text.text = text;
+        public void RegisterBackButtonCallback(UnityAction callback) => _backButton.RegisterNewCallback(callback);
+        public void Localize(UILocalisation localisation) => _backButton.GetComponentInChildren<TextMeshProUGUI>().text = localisation.BackButton;
+        public void SetActivePanel(bool isActive) => _panel.SetActive(isActive);
     }
 }
